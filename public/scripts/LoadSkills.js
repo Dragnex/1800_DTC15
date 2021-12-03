5001bb6837d2f34887dc2e3d6db3b971fe5eb409
Alex Pu <alexander.r.pu@gmail.com>
2021-12-03 11:48:34 -0800

    changed counter

--- public/scripts/LoadSkills.js
+++ public/scripts/LoadSkills.js
@@ -6,16 +6,16 @@ function PopulateSkills(user, template){
         for (let [key, value] of Object.entries(userData.data()["Skills"])){
             //htmlObject = document.createElement('div');
             //htmlObject.setAttribute("class", "list-group-item");
-            interval = value["interval"];
+            interval = value["interval"].toLowerCase();
             let logs = userData.data()["Skills"][key]["iterations"];
-            let intervalObj = {"weekly": 604800000, "monthly": 2419000000, "daily": 86400000}
-            let done = 0;
-            for (let [objKey, objValue] of Object.entries(logs)) {
-                console.log(timestamp, parseInt(objKey));
-                if (timestamp - parseInt(objKey) < intervalObj[value["interval"]]){
-                    done++;
-                }
-            }
+            let logKeys = GetSortedObjectKeys(logs)
+            // for (let [objKey, objValue] of Object.entries(logs)) {
+            //     console.log(timestamp, parseInt(objKey));
+            //     if (timestamp - parseInt(objKey) < intervalObj[value["interval"]]){
+            //         done++;
+            //     }
+            // }
+            let done = GetStreakLength(logs, key, value, interval, logKeys)
             currentSkill = skillTemplate.replaceAll("FREQUENCY", interval).replaceAll("TITLE", key).replaceAll("AMOUNT", done);
             document.getElementById("table-of-streaks").innerHTML += currentSkill;
             //htmlObject.innerHTML = currentSkill;
@@ -54,4 +54,27 @@ function RedirectStreakDetails(id, id2){
     console.log(id, id2);
     setActiveStreak(id, id2);
     window.location.assign("log-activity.html");
+}
+
+function GetStreakLength(logs, key, value, interval, logKeys) {
+    let intervalObj = {"weekly": 604800000, "monthly": 2419000000, "daily": 86400000}
+    // intervalObj = {"weekly": 7, "monthly": 30, "daily": 1}
+    let streak = 0
+    let todayDone = false;
+    let timestamp = new Date().getTime();
+    let lastInterval = Math.floor(parseInt(timestamp) / parseInt(intervalObj[interval]))
+    for (let objKey of logKeys) {
+        let logInterval = Math.floor(parseInt(objKey) / parseInt(intervalObj[interval]))
+        console.log(Math.floor((logInterval - lastInterval)));
+        if (Math.floor((logInterval - lastInterval) == -1)){
+            streak++;
+            lastInterval = logInterval;
+        }
+        else if (!todayDone && (Math.floor((logInterval - lastInterval) == 0))){
+            streak++;
+            lastInterval = logInterval;
+            todayDone = true
+        }
+    }
+    return streak
 }
